4ebb72c4f38abc8309151ee87ab3d720f424d86e
gaksss <clement.carlier2@gmail.com>
2024-11-14 11:10:58 +0100

    Exo3 fini

--- script.js
+++ script.js
@@ -7,19 +7,29 @@
 
 // EXERCICE 2
 
-const heroes = [
-  "Spider-Man",
-  "Batman",
-  "Wonder Woman",
-  "Iron Man",
-  "Superman",
-  "Captain America",
-  "Black Panther",
-  "Flash",
-  "Hulk",
-  "Thor",
-];
+// const heroes = [
+//   "Spider-Man",
+//   "Batman",
+//   "Wonder Woman",
+//   "Iron Man",
+//   "Superman",
+//   "Captain America",
+//   "Black Panther",
+//   "Flash",
+//   "Hulk",
+//   "Thor",
+// ];
 
-heroes.forEach((hero) => {
-    console.log("Mon héro préféré est " + hero)
-})
+// heroes.forEach((hero) => {
+//     console.log("Mon héro préféré est " + hero)
+// })
+
+// EXERCICE 3
+
+// let count = 10;
+
+// while (count >= 0) {
+//   console.log(count);
+//   count = count - 1;
+// }
+// console.log("Décollage !");
